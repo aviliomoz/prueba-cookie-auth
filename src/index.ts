7085ef3a9f9397fe17fa581ad1d0d8f9e3caf13c
aviliomoz <aviliomuoz@gmail.com>
2024-10-07 15:31:38 -0500

    first

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = 3000;
+
+app.post("/test/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { test } = req.cookies;
+
+  if (test) {
+    res.status(200).json({ message: "La cookie ya esta ocupada" });
+    return;
+  }
+
+  res.cookie("test", id, {
+    httpOnly: true,
+  });
+
+  res.status(200).json({ message: "Cookie establecida correctamente" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server ready");
+});
